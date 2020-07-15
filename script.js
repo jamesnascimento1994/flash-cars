8068e6dd324f8073d777bbb9e2ea6acd2983ba78
jamesnascimento1994 <james.nascimento94@yahoo.com>
2020-07-15 13:00:49 -0400

    Randomize, and some other brush ups

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let currentQuestion = 0;
 let currentAnswer = 0;
+let currentRightScore = 0;
+let currentWrongScore = 0;
 let startButton = document.querySelector('.start-button');
 let score = document.querySelector('.score');
 startButton.addEventListener('click', startGame);
-
+let scoreRight = document.querySelector('.score-right');
+let scoreWrong = document.querySelector('.score-wrong')
 let question = document.querySelector('.question');
 let answer = document.querySelector('.answer');
-let scoreRight = document.querySelector('.score-right');
-let scoreWrong = document.querySelector('.score-wrong');
 let flashcard = document.querySelector('#flashcard');
-// let cardStack1 = document.querySelector('.card-stack-1');
-// let cardStack2 = document.querySelector('.card-stack-2');
-// let cardStack2 = document.querySelector('.card-stack-3');
 let form = document.querySelector('form');
 let userInput = document.querySelector('.input');
+let rightScore = 10;
+let wrongScore = 5;
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     let answer = cards[currentAnswer];
@@ -22,23 +22,26 @@ form.addEventListener('submit', function (e) {
     currentAnswer = currentAnswer + 1;
     // console.log(currentAnswer);
 	if (answer.a === userInput.value) {
-		alert('Correct!');
-	// 	right.textContent++;
-	// 	userInput.value = null;
+        document.querySelector('#flashcard').innerText = 'Correct!'
+        scoreRight.innerText = ++currentRightScore;
 	} else {
-	    alert('Incorrect! ' + answer.a);
-	// 	wrong.textContent++;
-	// 	userInput.value = null;
+        document.querySelector('#flashcard').innerText = 'Incorrect!';
+        scoreWrong.innerText = ++currentWrongScore;
+}
+    if (rightScore === currentRightScore) {
+        alert('You have won the game!');
+    } else if (wrongScore === currentWrongScore) {
+        alert('You get nothing! You lose!');
     }
-    // flashcard.innerText = answer.a;
 });
 let text = document.querySelector('.text');
 
 let nextCard = document.querySelector('.next-card');
 nextCard.addEventListener('click', theNextCard);
 function theNextCard() {
-	// message.style.display = 'none';
     showQuestion();
+    userInput.value = null;
+    
 }
 
 let questionContainer = document.querySelector('.question-container');
@@ -54,15 +57,25 @@ var cards = [
 	{ q: 'Outback', a: 'Subaru' },
 	{ q: 'Escape', a: 'Ford' },
 	{ q: 'Sierra', a: 'GMC' },
-	{ q: 'Jetta', a: 'Volkswagen' },
+    { q: 'Jetta', a: 'Volkswagen' },
+    { q: 'Benz', a: 'Mercedes'},
+    { q: 'M4', a: 'BMW'},
+    { q: 'Sorento', a: 'KIA'},
 ];
+
+for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        const temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+
 
 function startGame(e) {
 	e.preventDefault();
 	score.style.display = 'block';
 	startButton.style.display = 'none';
-    flashcard.style.display = 'block';
-	showQuestion();
+    showQuestion();
 }
 
 function showQuestion() {
@@ -71,5 +84,11 @@ function showQuestion() {
 	currentQuestion = currentQuestion + 1;
 	console.log(currentQuestion);
     flashcard.innerText = question.q;
-    
+
+}
+
+function endGame(e) {
+e.preventDefault();
+for (i = 0; i < q[9].length; i++) {
+}
 }
